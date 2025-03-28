85f91657b65490baa3b23e26fc18a083eab1a7b5
현승훈 <tmdgns4970@handong.ac.kr>
2025-03-28 13:22:09 +0900

    Second HomeWork

---
+++ practice2/practice2/practice2App.swift
@@ -0,0 +1,17 @@
+//
+//  practice2App.swift
+//  practice2
+//
+//  Created by 현승훈 on 3/21/25.
+//
+
+import SwiftUI
+
+@main
+struct practice2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ practice2/practice2Tests/practice2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  practice2Tests.swift
+//  practice2Tests
+//
+//  Created by 현승훈 on 3/21/25.
+//
+
+import Testing
+@testable import practice2
+
+struct practice2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
